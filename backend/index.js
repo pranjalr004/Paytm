cefcfde5c83cb8fe3b5a5736784a8ab5c8a2c7a1
pranjalr004 <pranjalr376@gmail.com>
2025-01-13 23:03:24 +0530

    routes added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express=require("express")
 const cors=require("cors")
 
-const app=express()
-const mainRouter=require("./")
+const mainRouter=require("./routes/index")
+app.use(cors())
+app.use(express.json())
 
+const app=express()
 app.use("/api/v1",mainRouter)
+
+
+app.listen(3000)
